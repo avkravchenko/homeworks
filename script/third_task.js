eb0e08b1f82d5a1fe4879b488b9f77afe7931f24
Alexander <sashakravchenko1996@gmail.com>
2022-12-10 00:07:59 +0600

    third task is done

---
+++ script/third_task.js
@@ -0,0 +1,32 @@
+/*Задание 3.
+
+Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".*/
+
+//first option
+
+let x = "example";
+
+let splitx = x.split('');
+
+let reversesplit = splitx.reverse();
+
+let complete = reversesplit.join('');
+
+console.log(complete);
+
+//second option
+
+let ourString = "murder";
+
+function reverseStr(ourString) {
+    let result = "";
+    for (let i = ourString.length - 1; i >= 0; i--) {
+        result += ourString[i];
+    }
+    return result;
+}
+
+console.log(reverseStr(ourString));
+
+
+
